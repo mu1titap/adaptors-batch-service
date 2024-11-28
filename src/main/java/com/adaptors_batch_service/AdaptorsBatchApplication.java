19734e1b00fce1f294af4056248a9f0c87b3a1b2
Jung-Hey <gjgmlwjd9@gmail.com>
2024-11-28 11:16:29 +0900

    create batch service

---
+++ src/main/java/com/adaptors_batch_service/AdaptorsBatchApplication.java
@@ -0,0 +1,13 @@
+package com.adaptors_batch_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptorsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaptorsBatchApplication.class, args);
+	}
+
+}
