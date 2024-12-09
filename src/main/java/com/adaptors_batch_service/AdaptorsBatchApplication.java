86ab7a97a87c990f0a7b718214f1f6f09ebe4a2c
Jung-Hey <gjgmlwjd9@gmail.com>
2024-12-09 13:17:01 +0900

    스케줄러 어노테이션 추가

--- src/main/java/com/adaptors_batch_service/AdaptorsBatchApplication.java
+++ src/main/java/com/adaptors_batch_service/AdaptorsBatchApplication.java
@@ -3,9 +3,11 @@ package com.adaptors_batch_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
 public class AdaptorsBatchApplication {
 
 	public static void main(String[] args) {
