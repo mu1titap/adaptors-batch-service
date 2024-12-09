a7bdbce880a56d424babcd6deb81b96b3a07a4f2
Jung-Hey <gjgmlwjd9@gmail.com>
2024-12-09 20:27:15 +0900

    feat8

--- src/main/java/com/adaptors_batch_service/AdaptorsBatchApplication.java
+++ src/main/java/com/adaptors_batch_service/AdaptorsBatchApplication.java
@@ -2,11 +2,10 @@ package com.adaptors_batch_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableScheduling
 public class AdaptorsBatchApplication {
 
