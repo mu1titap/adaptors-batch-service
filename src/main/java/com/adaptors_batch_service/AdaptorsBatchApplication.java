d241fcceccf851e0a286cf27b2436a0bef4104b8
Jung-Hey <gjgmlwjd9@gmail.com>
2024-12-14 15:49:56 +0900

    cd test

--- src/main/java/com/adaptors_batch_service/AdaptorsBatchApplication.java
+++ src/main/java/com/adaptors_batch_service/AdaptorsBatchApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
 @EnableScheduling
 public class AdaptorsBatchApplication {
 
